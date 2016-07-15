40b428aab8f0872a1e59a4dd4cf24af7f95abd4d
tazim <tazimtazim2012@hotmail.com>
2016-07-15 10:06:37 +0600

    Basic File Structure setup

--- src/root/App/Main.java
+++ src/root/App/Main.java
@@ -1,16 +1,13 @@
-package sample;
+package root.App;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        
+
     }
 
 
